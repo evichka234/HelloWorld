d2286e31cab8a95517c99bdbb9398a45bf6c6077
ewunia <ewunia@DESKTOP-DJTNI17>
2023-11-22 22:16:35 +0100

    Add project files.

---
+++ HelloWorld/test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class test
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("My name is Ewa");
+        }
+
+    }
+}
